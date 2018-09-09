120fa2734e997ace2d46a50a3a9c707fe7d09f93
Timothy Schindler <timothy.schindler1@marist.edu>
2018-09-09 14:03:30 -0400

    Solution to Mad Libs exercise.

---
+++ Madlibs.py
@@ -0,0 +1,11 @@
+#CMPT 120L 113
+#Madlibs
+#Author: Timothy Schindler
+#Created: 9/6/18
+
+obj = input('Enter an object: ')
+verb = input('Enter a verb: ')
+adj = input('Enter a adjective: ')
+place = input('Enter a place: ')
+
+print("Take your " + adj + " " + obj + " and" + " " + verb + " it at the " + place)

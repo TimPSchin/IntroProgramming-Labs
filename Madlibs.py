62a92abcaac77d25e373b0cd53c88261bd5be0c5
Timothy Schindler <timothy.schindler1@marist.edu>
2018-09-13 14:27:48 -0400

    I added the three functions

--- Madlibs.py
+++ Madlibs.py
@@ -3,9 +3,19 @@
 #Author: Timothy Schindler
 #Created: 9/6/18
 
-obj = input('Enter an object: ')
-verb = input('Enter a verb: ')
-adj = input('Enter a adjective: ')
-place = input('Enter a place: ')
+def promptForWords():
+    global obj, verb, adj, place
+    obj = input('Enter an object: ')
+    verb = input('Enter a verb: ')
+    adj = input('Enter a adjective: ')
+    place = input('Enter a place: ')
 
-print("Take your " + adj + " " + obj + " and" + " " + verb + " it at the " + place)
+def makeAndPrintSentence():
+    print("Take your " + adj + " " + obj + " and" + " " + verb + " it at the " + place)
+
+def main():
+    promptForWords()
+    makeAndPrintSentence()
+
+
+main()

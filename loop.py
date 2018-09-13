7739d32ec17e2742086ad7f586215fa690265b79
Timothy Schindler <timothy.schindler1@marist.edu>
2018-09-13 14:12:59 -0400

    Added call function

--- loop.py
+++ loop.py
@@ -3,7 +3,10 @@
 #Author: Timothy Schindler
 #Created: 9/6/18
 
-x = 2
-while x <= 20:
-    print(x)
-    x += 2
+def main():
+    x = 2
+    while x <= 20:
+        print(x)
+        x += 2
+
+main()

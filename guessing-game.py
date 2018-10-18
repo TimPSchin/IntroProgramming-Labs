a65d7a6d8e4d8606c0785e79351301279bcc4e2b
Timothy Schindler <timothy.schindler1@marist.edu>
2018-10-18 14:35:09 -0400

    Initial version of a simple game.

---
+++ guessing-game.py
@@ -0,0 +1,19 @@
+def main():
+    print("PYTHON GUESSING GAME")
+    print(gameLoop())
+        
+answer = "monkey"
+x = 0
+def gameLoop():
+    global x
+    print("I am thinking of an animal.")
+    while x < 10:
+        guess = input("\nGuess what animal it is?").lower().strip()
+        x += 1
+        if guess == answer:
+              return "\nCongratulations, you guessed correctly!"
+        else:
+              print("I am sorry, thats not it, try again")
+    print("\nDamn you suck")
+
+main()
